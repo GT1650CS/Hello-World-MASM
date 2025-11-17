f835b63fffaa511506e69a081eb0c592d23a5692
GT1650CS <hotlap8878@gmail.com>
2025-11-17 20:02:04 +0000

    Update hello.asm - clarified comment

--- x64/hello.asm
+++ x64/hello.asm
@@ -10,7 +10,11 @@ INCLUDE windows.inc
 ; --- CODE ---
 .CODE
 main PROC
-	  SUB RSP, 8 ; simulate normal caller state
+	; the invoke macro assumes normal callee state. 
+	; However, with a direct entrypoint, RSP is 0, as 
+	; opposed to 8 when called normally. Therefore, we
+	; subtract 8 to simulate regular callee state.
+	SUB RSP, 8 
     INVOKE MessageBoxA, 0, OFFSET msg, OFFSET cap, MB_OK
     INVOKE ExitProcess, 0
 main ENDP
